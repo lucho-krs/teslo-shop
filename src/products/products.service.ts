0731917d70654c897298b70ec1001a1a4966b04a
Luis Cereño <luis.cereno.t@gmail.com>
2024-03-06 22:04:39 -0300

    Se crea eliminacion en cascada

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -137,4 +137,17 @@ export class ProductsService {
     this.logger.error( error );
     throw new InternalServerErrorException('Unexpected error, check server logs');
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query
+        .delete()
+        .where({})
+        .execute();
+    } catch (error) {
+      this.handleDBExceptions( error );
+    }
+  }
 }
