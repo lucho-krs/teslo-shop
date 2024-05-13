70048f8f71534067e47402d1d7dbd4675e430295
Luis Cereño <luis.cereno.t@gmail.com>
2024-05-13 16:11:46 -0400

    Se crea relación entre usuario y producto

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -8,7 +8,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth( ValidRoles.admin )
+  // @Auth( ValidRoles.admin )
   findAll() {
     return this.seedService.runSeed();
   }
